af3521dfd78af1ea4d5a1935d7af32ffa34fbdc2
Neeraj <neeraj@neeraj.(none)>
2016-10-16 11:29:33 +0530

    project compiled in lappy with changes suggested by cli

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,4 @@
 import { Component } from '@angular/core';
-import { Router, ActivatedRoute, Params } from '@angular/router';
-import './rxjs-operators'
 
 @Component({
   selector: 'app-root',
@@ -9,22 +7,4 @@ import './rxjs-operators'
 })
 export class AppComponent {
   title = 'app works!';
-  toggleNavBar: boolean;
-
-  constructor(
-    private router:Router
-  ){
-    this.toggleNavBar = false;
-  }
-
-  setNavClass(){
-    return {
-      "hide": false //this.toggleNavBar
-    }
-  }
-
-  toggle_search_bar(){
-    this.toggleNavBar = !this.toggleNavBar;
-    this.router.navigate(['/search']);
-  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,36 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpModule, JsonpModule } from '@angular/http';
-
-import { MaterializeModule } from 'angular2-materialize';
-
-import { routing } from './app.routes';
+import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
-import { ListsComponent } from './lists/lists.component';
-import { SearchComponent } from './search/search.component';
-import { StatsComponent } from './stats/stats.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    ListsComponent,
-    SearchComponent,
-    StatsComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule,
-    JsonpModule,
-    MaterializeModule,
-
-    routing
+    HttpModule
   ],
-  providers: [
-
-  ],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
